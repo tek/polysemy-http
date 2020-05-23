f91dd46290181020d9e1f167e136c2e07d3c793a
Torsten Schmits <torstenschmits@gmail.com>
2020-05-23 15:28:17 +0200

    bump string-interpolate

--- ops/nix/overrides.nix
+++ ops/nix/overrides.nix
@@ -10,6 +10,7 @@ let
     (pack "polysemy" "1.3.0.0" "1p75i56qpl0v79vrlzw04117czzgwhn1l0vadvka8m7drmcvwsf6")
     (pack "co-log-polysemy" "0.0.1.1" "0frp0p4nk37vdqmgnw851v6z1185rhvwsfl973dc1gw4jvmfd8b8")
     (pack "polysemy-plugin" "0.2.5.0" "0jnps8kwxd0hakis5ph77r45mv1qnkxdf5506shcjb1zmxqmxpjv")
+    (pack "string-interpolate" "0.2.1.0" "0747bfzafcrrjl7grisv1cbvq1fblzmn37r9daknbnm988h9wann")
   ];
   versionOverrides = builtins.listToAttrs versions;
 in versionOverrides
