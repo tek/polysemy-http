580104dc34a46283ade01b353a0763b09af4b72e
Torsten Schmits <torstenschmits@gmail.com>
2020-07-11 21:46:34 +0200

    bump tryp-hs

--- ops/nix/default.nix
+++ ops/nix/default.nix
@@ -2,7 +2,7 @@
   base,
 }:
 let
-  hs = import (fetchTarball "https://gitlab.tryp.io/nix/hs/-/archive/0974d702203f5c7bba905808fd598591cf90f571.tar.gz");
+  hs = import (fetchTarball "https://gitlab.tryp.io/nix/hs/-/archive/9ca9ed9b741508edb96c397197cebf7da417b3d5.tar.gz");
   packages = { http-client-polysemy = base + /packages/http-client-polysemy; };
   overrides = import ./overrides.nix { inherit (hs) hackage; };
   project = hs.project {
