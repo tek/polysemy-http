4d358b94594b641390a1d23c20380b0e7a915bc5
Torsten Schmits <torstenschmits@gmail.com>
2020-08-21 23:42:18 +0200

    use niv

--- ops/nix/default.nix
+++ ops/nix/default.nix
@@ -2,17 +2,22 @@
   base,
 }:
 let
-  hsSrc = fetchTarball "https://gitlab.tryp.io/nix/hs/-/archive/a1e7b43e1270518c9af311c736349c56f226e73c.tar.gz";
+  niv = import "${toString base}/nix/sources.nix";
+  nixpkgsSrc = niv.nixpkgs;
   # hsSrc = ../../../../nix/tryp-hs;
+  hsSrc = niv.tryp-hs;
+
+  nixpkgs = import nixpkgsSrc;
   hs = import hsSrc { inherit base; };
+
   packages = { polysemy-http = base + /packages/polysemy-http; };
+
   project = hs.project {
-    inherit packages base;
+    inherit nixpkgs packages base;
     compiler = "ghc8101";
-    overrides = import ./overrides.nix;
+    overrides = import ./overrides.nix niv;
     ghciArgs = ["-hide-package" "base"];
     cabal2nixOptions = "--no-hpack";
     options_ghc = "-fplugin=Polysemy.Plugin";
   };
-  tags = hs.util.tags { packages = project.sets.all; inherit (project) ghc; packageDir = "packages"; };
-in tags // project
+in project
--- ops/nix/overrides.nix
+++ ops/nix/overrides.nix
@@ -1,3 +1,4 @@
+niv:
 {
   pkgs,
   hackage,
@@ -6,22 +7,13 @@ self: super:
 let
   inherit (hackage) pack thunk cabal2nix subPkg github;
 
-  base-noprelude = github {
-    owner = "tek";
-    repo = "base-noprelude";
-    rev = "d6ad765adca25c2efb06110dae854113fe1cd733";
-    sha256 = "0ffv2i29q05p5jza2jl1jzrfg0z7z6fw4ix7ay4h4ppzkzakk0ib";
-  };
-
   versions = [
   ];
   versionOverrides = builtins.listToAttrs versions;
 
-  polysemySrc = thunk "polysemy";
   custom = {
-    inherit base-noprelude;
-    polysemy = cabal2nix "polysemy" polysemySrc;
-    polysemy-plugin = subPkg "polysemy-plugin" "polysemy-plugin" polysemySrc;
+    polysemy = cabal2nix "polysemy" niv.polysemy;
+    polysemy-plugin = subPkg "polysemy-plugin" "polysemy-plugin" niv.polysemy;
   };
 in
   versionOverrides // custom
