4f7e7681a301ff9e6130b3704ed68ec10c3b7b7c
Torsten Schmits <torstenschmits@gmail.com>
2020-09-03 19:14:02 +0200

    keep hpack file out of the package dir

--- ops/hpack.zsh
+++ ops/hpack.zsh
@@ -3,13 +3,17 @@
 
 setopt err_exit no_unset
 
-args=$*
 base=${0:a:h:h}
+hpack="$base/ops/hpack"
 
 gen()
 {
-  pushd $1
-  hpack $args
+  local dir=$1
+  pushd $dir
+  cp $hpack/packages/${dir:t}.yaml $dir/package.yaml
+  ln -srf $hpack/shared $dir/shared
+  trap "rm -f $dir/package.yaml $dir/shared" EXIT
+  hpack 1>/dev/null
   popd
 }
 
