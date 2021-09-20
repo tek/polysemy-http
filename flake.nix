295a633622787a3189d0b7ccd0e33442ac8c68af
Torsten Schmits <git@tryp.io>
2021-09-20 21:21:26 +0200

    update dependencies

--- flake.nix
+++ flake.nix
@@ -8,35 +8,23 @@
     common = { hackage, jailbreak, ... }: {
       path = hackage "0.9.0" "14symzl1rszvk5zivv85k79anz7xyl5gaxy0sm4vhhzsgxc59msv";
       path-io = jailbreak (hackage "1.6.3" "05hcxgyf6kkz36mazd0fqwb6mjy2049gx3vh8qq9h93gfjkpp2vc");
-      polysemy = hackage "1.6.0.0" "15k51ysrfcbkww1562g8zvrlzymlk2rxhcsz9ipsb0q6h571qgvf";
-      polysemy-plugin = hackage "0.4.0.0" "0pah1a8h8ckbv2fq20hrikrd1p5a3bdxr03npkyixc6mv5k1rmck";
-      relude = hackage "1.0.0.1" "164p21334c3pyfzs839cv90438naxq9pmpyvy87113mwy51gm6xn";
-      servant = hackage "0.18.3" "0dklk3i3bcg85rzrmiggkypkw7pwkh6m5dlrjipsw0njfmlhqfz6";
-      servant-client = hackage "0.18.3" "1gz2a0h9xnc1hmfp5bv84pg7vi45b9rvjk1fgy51zizg8vdlagvg";
-      servant-client-core = hackage "0.18.3" "1x0f3kalzrwj2blgsmk269m37bb7sygw6lr7dbp0rk6jbrfrkjm1";
-      servant-server = hackage "0.18.3" "0388v7drjj0gph99kszxivi4qijbckys67bkbv6384g2zrdplvj7";
     };
 
     compat901 = { hackage, jailbreak, minimal, ... }: {
-      cryptonite = hackage "0.29" "1zkw80a6iw8ih33355cfffbyg2md1cn7pcpwnnvr34p0xsykxdrl";
-      memory = hackage "0.16.0" "0ix5zdq3c51k959y2n4dfka4dnfjp710c6bms0pk3vhzhxl3q1nh";
-      http-client = hackage "0.7.8" "16xil9wdx82wxk7h1ilk0xyb1bkq51rpvvvr1c61j4nkc8v4878v";
-      retry = hackage "0.9.0.0" "09z9vdn656kmkd593m13791v7y75w51ggwv51aw73lilbpas2ba0";
-      type-errors-pretty = jailbreak;
-      typerep-map = jailbreak (hackage "0.3.3.0" "15i0h2xczf4x898vjd4vgbb8n10gbsbvy2s2pfw4b3vzf0a1rayl");
-      mono-traversable = minimal (hackage "1.0.15.1" "10rbsnawplg501sj6b4366dxah8lsfpi39scg78b1rxbhh7cwqcr");
+      relude = hackage "1.0.0.1" "164p21334c3pyfzs839cv90438naxq9pmpyvy87113mwy51gm6xn";
     };
 
     compat = { hackage, ... }: {
-      polysemy = hackage "1.5.0.0" "1xl472xqdxnp4ysyqnackpfn6wbx03rlgwmy9907bklrh557il6d";
-      polysemy-conc = hackage "0.1.0.3" "0g4z20il8l2hgg2m6vmc6mk6c1x7rml57q4fg9gnri06vavsxy5n";
-      polysemy-plugin = hackage "0.3.0.0" "1frz0iksmg8bpm7ybnpz9h75hp6hajd20vpdvmi04aspklmr6hj0";
-      polysemy-log = hackage "0.2.2.2" "0dmsyi970ddl3ihvm51cbdmlhnkl2c7lkjhwxgbk8ilf1hhd1r72";
-      polysemy-test = hackage "0.3.1.6" "0bfh37l68a5chhjfr7gqcffsmvdgg5hqclxi0fc5xnqni2mg81ak";
-      polysemy-time = hackage "0.1.3.1" "1ldg92dmy1nyjhkbmh5k32q94pn2c7qcfjc4yhl4lc1wnfp6r59m";
+      polysemy = hackage "1.6.0.0" "15k51ysrfcbkww1562g8zvrlzymlk2rxhcsz9ipsb0q6h571qgvf";
+      polysemy-conc = hackage "0.3.0.0" "0vqp4q3pgxgf5azr87vmlk5irxdd7491gayp7n74373nhrndfd45";
+      polysemy-plugin = hackage "0.4.0.0" "0pah1a8h8ckbv2fq20hrikrd1p5a3bdxr03npkyixc6mv5k1rmck";
+      polysemy-log = hackage "0.2.2.4" "1fgn7ywifbp02lz2wyaixvp43vnrff8n5nkczxmq1r5bzqbs6f45";
+      polysemy-test = hackage "0.3.1.7" "0j33f5zh6gyhl86w8kqh6nm02915b4n32xikxc4hwcy7p5l7cl34";
+      polysemy-time = hackage "0.1.4.0" "0hwx89cilmsdjs3gb5w6by87ysy24scgj5zg77vbfnqpzr3ifrwh";
     };
 
     main = { hackage, ... }: {
+      relude = hackage "1.0.0.1" "164p21334c3pyfzs839cv90438naxq9pmpyvy87113mwy51gm6xn";
       tasty-hedgehog = hackage "1.1.0.0" "0cs96s7z5csrlwj334v8zl459j5s4ws6gmjh59cv01wwvvrrjwd9";
     };
   in
@@ -47,5 +35,6 @@
     compatOverrides = { all = compat; ghc901 = [common compat901]; };
     ghci.extraArgs = ["-fplugin=Polysemy.Plugin"];
     versionFile = "ops/hpack/packages/polysemy-http.yaml";
+    ghcid.easy-hls = false;
   };
 }
