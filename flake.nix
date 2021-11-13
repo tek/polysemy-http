39c98919ca126d84dd939b69441392d5a0492e56
Torsten Schmits <git@tryp.io>
2021-11-14 00:43:54 +0100

    clean up deps

--- flake.nix
+++ flake.nix
@@ -5,34 +5,30 @@
 
   outputs = { hix, ... }:
   let
-    common = { hackage, jailbreak, ... }: {
-      path = hackage "0.9.0" "14symzl1rszvk5zivv85k79anz7xyl5gaxy0sm4vhhzsgxc59msv";
-      path-io = jailbreak (hackage "1.6.3" "05hcxgyf6kkz36mazd0fqwb6mjy2049gx3vh8qq9h93gfjkpp2vc");
-    };
 
-    compat901 = { hackage, jailbreak, minimal, ... }: {
-      relude = hackage "1.0.0.1" "164p21334c3pyfzs839cv90438naxq9pmpyvy87113mwy51gm6xn";
-    };
-
-    compat = { hackage, ... }: {
+    all = { hackage, jailbreak, ... }: {
       polysemy = hackage "1.6.0.0" "15k51ysrfcbkww1562g8zvrlzymlk2rxhcsz9ipsb0q6h571qgvf";
-      polysemy-conc = hackage "0.3.0.0" "0vqp4q3pgxgf5azr87vmlk5irxdd7491gayp7n74373nhrndfd45";
       polysemy-plugin = hackage "0.4.0.0" "0pah1a8h8ckbv2fq20hrikrd1p5a3bdxr03npkyixc6mv5k1rmck";
-      polysemy-log = hackage "0.2.2.4" "1fgn7ywifbp02lz2wyaixvp43vnrff8n5nkczxmq1r5bzqbs6f45";
-      polysemy-test = hackage "0.3.1.7" "0j33f5zh6gyhl86w8kqh6nm02915b4n32xikxc4hwcy7p5l7cl34";
-      polysemy-time = hackage "0.1.4.0" "0hwx89cilmsdjs3gb5w6by87ysy24scgj5zg77vbfnqpzr3ifrwh";
     };
 
-    main = { hackage, ... }: {
+    ghc901 = { hackage, jailbreak, minimal, ... }: {
       relude = hackage "1.0.0.1" "164p21334c3pyfzs839cv90438naxq9pmpyvy87113mwy51gm6xn";
-      tasty-hedgehog = hackage "1.1.0.0" "0cs96s7z5csrlwj334v8zl459j5s4ws6gmjh59cv01wwvvrrjwd9";
     };
+
+    ghc884 = { hackage, unbreak, jailbreak, ... }: {
+      polysemy-conc = hackage "0.5.0.0" "0dv3naixsv8fbwqdfa4j2wg1xjlrk1w80v6wclg96rlaisxgsxlg";
+      polysemy-test = hackage "0.3.1.7" "0j33f5zh6gyhl86w8kqh6nm02915b4n32xikxc4hwcy7p5l7cl34";
+      polysemy-log = hackage "0.3.0.1" "0xjz5wqashhyr2didmdxg69mbk6jgmq6wwgj7w82mj8bdwdvbkm0";
+      polysemy-resume = hackage "0.2.0.0" "0kh7cwqkr5w69zkm68l6q4d8nkai7fc29n48p3f8skqw638x4w9p";
+      polysemy-time = hackage "0.1.4.0" "0hwx89cilmsdjs3gb5w6by87ysy24scgj5zg77vbfnqpzr3ifrwh";
+      relude = hackage "1.0.0.1" "164p21334c3pyfzs839cv90438naxq9pmpyvy87113mwy51gm6xn";
+    };
+
   in
   hix.flake {
     base = ./.;
     packages.polysemy-http = ./packages/polysemy-http;
-    overrides = [compat common main];
-    compatOverrides = { all = compat; ghc901 = [common compat901]; };
+    overrides = { inherit all ghc901 ghc884; };
     ghci.extraArgs = ["-fplugin=Polysemy.Plugin"];
     versionFile = "ops/hpack/packages/polysemy-http.yaml";
     ghcid.easy-hls = false;
